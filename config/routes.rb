1ff6456a6ce21c50e8cab2cafc87b016d19e23b8
Bharat Rathor <bharat.rathor.5074@gmail.com>
2023-05-19 17:16:11 +0530

    Create api for reciepe
    rails generate controller api/v1/Recipes index create show destroy  --skip-template-engine --no-helper

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      get 'recipes/index'
+      get 'recipes/create'
+      get 'recipes/show'
+      get 'recipes/destroy'
+    end
+  end
+  get 'homepage/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "homepage#index"
 end
